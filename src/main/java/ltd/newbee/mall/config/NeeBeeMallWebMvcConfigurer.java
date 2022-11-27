bb82a1dab4790576aaaf9e29cf2bff97d5b3355f
WangMinan <wangminan0811@hotmail.com>
2022-11-27 17:13:22 +0800

    保险起见我还是重置了一下他的跨域请求

--- src/main/java/ltd/newbee/mall/config/NeeBeeMallWebMvcConfigurer.java
+++ src/main/java/ltd/newbee/mall/config/NeeBeeMallWebMvcConfigurer.java
@@ -58,8 +58,19 @@ public class NeeBeeMallWebMvcConfigurer extends WebMvcConfigurationSupport {
      */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOriginPatterns("*")
-                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowCredentials(true).maxAge(3600);
+        // 设置允许跨域的路径
+        registry.addMapping("/**")
+                // 设置允许跨域请求的域名
+                .allowedOriginPatterns("*")
+                // 是否允许cookie
+                .allowCredentials(true)
+                // 设置允许的请求方式
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE","OPTIONS")
+                // 设置允许的header属性
+                .allowedHeaders("*")
+                // 我们这次不带token 只在前端挂载路由守卫 所以可以直接写*
+                .allowedOriginPatterns("*")
+                // 跨域允许时间
+                .maxAge(3600);
     }
 }
