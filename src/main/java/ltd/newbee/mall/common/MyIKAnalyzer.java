7d0373108512f5d76595de99b1c821c6c440a40d
Rookie-Bugmaker <87959670+Rookie-Bugmaker@users.noreply.github.com>
2022-12-13 15:01:11 +0800

    自定义IK分词器

---
+++ src/main/java/ltd/newbee/mall/common/MyIKAnalyzer.java
@@ -0,0 +1,32 @@
+package ltd.newbee.mall.common;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.Tokenizer;
+
+public class MyIKAnalyzer extends Analyzer {
+
+    private boolean useSmart;
+
+    public MyIKAnalyzer() {
+        this(false);
+    }
+
+    public MyIKAnalyzer(boolean useSmart) {
+        this.useSmart = useSmart;
+    }
+
+    @Override
+    protected TokenStreamComponents createComponents(String s) {
+        Tokenizer _MyIKTokenizer = new MyIKTokenizer(this.useSmart());
+        return new TokenStreamComponents(_MyIKTokenizer);
+    }
+
+    public boolean useSmart() {
+        return this.useSmart;
+    }
+
+    public void setUseSmart(boolean useSmart) {
+        this.useSmart = useSmart;
+    }
+
+}
---
+++ src/main/java/ltd/newbee/mall/common/MyIKTokenizer.java
@@ -0,0 +1,53 @@
+package ltd.newbee.mall.common;
+
+import org.apache.lucene.analysis.Tokenizer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
+import org.apache.lucene.analysis.tokenattributes.TypeAttribute;
+import org.wltea.analyzer.core.IKSegmenter;
+import org.wltea.analyzer.core.Lexeme;
+
+import java.io.IOException;
+
+public class MyIKTokenizer extends Tokenizer {
+
+    private IKSegmenter _IKImplement;
+    private final CharTermAttribute termAtt = (CharTermAttribute)this.addAttribute(CharTermAttribute.class);
+    private final OffsetAttribute offsetAtt = (OffsetAttribute)this.addAttribute(OffsetAttribute.class);
+    private final TypeAttribute typeAtt = (TypeAttribute)this.addAttribute(TypeAttribute.class);
+    private int endPosition;
+
+    //useSmart：设置是否使用智能分词。默认为false，使用细粒度分词，这里如果更改为TRUE，那么搜索到的结果可能就少的很多
+    public MyIKTokenizer(boolean useSmart) {
+        this._IKImplement = new IKSegmenter(this.input, useSmart);
+    }
+
+    @Override
+    public boolean incrementToken() throws IOException {
+        this.clearAttributes();
+        Lexeme nextLexeme = this._IKImplement.next();
+        if (nextLexeme != null) {
+            this.termAtt.append(nextLexeme.getLexemeText());
+            this.termAtt.setLength(nextLexeme.getLength());
+            this.offsetAtt.setOffset(nextLexeme.getBeginPosition(), nextLexeme.getEndPosition());
+            this.endPosition = nextLexeme.getEndPosition();
+            this.typeAtt.setType(nextLexeme.getLexemeTypeString());
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public void reset() throws IOException {
+        super.reset();
+        this._IKImplement.reset(this.input);
+    }
+
+    @Override
+    public final void end() {
+        int finalOffset = this.correctOffset(this.endPosition);
+        this.offsetAtt.setOffset(finalOffset, finalOffset);
+    }
+
+}
