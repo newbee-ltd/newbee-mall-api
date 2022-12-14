996991c71765cd715c68c107e7900f454bc733c6
Rookie-Bugmaker <87959670+Rookie-Bugmaker@users.noreply.github.com>
2022-12-14 21:24:28 +0800

    暂时保存

--- src/main/java/ltd/newbee/mall/dao/GoodsDao.java
+++ src/main/java/ltd/newbee/mall/dao/GoodsDao.java
@@ -1,6 +1,10 @@
 package ltd.newbee.mall.dao;
 
 import ltd.newbee.mall.entity.NewBeeMallGoods;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.TextField;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -11,6 +15,10 @@ import java.util.List;
 
 public class GoodsDao {
 
+    /**
+     * 采集数据
+     * @return
+     */
     public List<NewBeeMallGoods> getAll() {
 
 // 数据库链接
@@ -126,6 +134,60 @@ public class GoodsDao {
         }
 
         return list;
+
+    }
+
+
+    /**
+     * 将数据转化为Lucene文档
+     * @param goods
+     * @return
+     */
+    public List getDocuments(List<NewBeeMallGoods> goods){
+
+// Document对象集合
+
+        List docList = new ArrayList();
+
+// Document对象
+
+        Document doc = null;
+
+        for (NewBeeMallGoods good : goods) {
+
+// 创建Document对象，同时要创建field对象
+
+            doc = new Document();
+
+// 根据需求创建不同的Field
+
+            Field id = new TextField("goods_id", good.getGoodsId().toString(), Field.Store.YES);
+
+            Field name = new TextField("goods_name", good.getGoodsName(), Field.Store.YES);
+
+            Field intro = new TextField("goods_intro", good.getGoodsIntro(), Field.Store.YES);
+
+            Field img = new TextField("goods_cover_img", good.getGoodsCoverImg(), Field.Store.YES);
+
+            Field price = new TextField("selling_price", good.getSellingPrice().toString(), Field.Store.YES);
+
+// 把域(Field)添加到文档(Document)中
+
+            doc.add(id);
+
+            doc.add(name);
+
+            doc.add(intro);
+
+            doc.add(img);
+
+            doc.add(price);
+
+            docList.add(doc);
+
+        }
+
+        return docList;
 
     }
 
--- src/test/java/GoodsTest.java
+++ src/test/java/GoodsTest.java
@@ -1,7 +1,14 @@
 import ltd.newbee.mall.dao.GoodsDao;
 import ltd.newbee.mall.entity.NewBeeMallGoods;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
 import org.junit.jupiter.api.Test;
 
+import java.nio.file.FileSystems;
 import java.util.List;
 
 
@@ -17,6 +24,50 @@ public class GoodsTest {
         for (NewBeeMallGoods good : goods) {
 
             System.out.println("商品id:" + good.getGoodsId() + ",商品名称:" + good.getGoodsName());
+        }
+
+    }
+
+    @Test
+    public void createIndex(){
+
+        try {
+
+            GoodsDao dao = new GoodsDao();
+
+// 分析文档，对文档中的field域进行分词
+
+            Analyzer analyzer = new StandardAnalyzer();
+
+// 创建索引
+
+// 1) 创建索引库目录
+
+            Directory directory = FSDirectory.open(FileSystems.getDefault().getPath("E:\\Lucene\\indexDir"));
+            // 索引读取工具
+
+// 2) 创建IndexWriterConfig对象
+
+            IndexWriterConfig cfg = new IndexWriterConfig(Version.LATEST, analyzer);
+
+// 3) 创建IndexWriter对象
+
+            IndexWriter writer = new IndexWriter(directory, cfg);
+
+// 4) 通过IndexWriter对象添加文档对象(document)
+
+            writer.addDocuments(dao.getDocuments(dao.getAll()));
+
+// 5) 关闭IndexWriter
+
+            writer.close();
+
+            System.out.println("创建索引库成功");
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
         }
 
     }
